345185659aa79a337775bc2c739b5bcd20c57505
tayurus <tayurus@gmail.com>
2020-01-29 12:50:22 +0300

    Добавляет поддержку ie

--- src/components/CardItem/CardItem.js
+++ src/components/CardItem/CardItem.js
@@ -58,63 +58,95 @@ const getDescription = (status, filling, description) => {
   }
 };
 
-const CardItem = ({
-  type,
-  title,
-  filling,
-  servingsCount,
-  miceCount,
-  additions,
-  weight,
-  description,
-  status,
-  onSelect,
-  className
-}) => {
-  return (
-    <div className={`${b({ [status]: true })} ${className}`} onClick={onSelect}>
-      <div className={b("container")}>
-        <div className={b("top-wrapper")}>
-          <p className={b("type")}>{type}</p>
-          <h2 className={b("title-wrapper")}>
-            <span className={b("title-top")}>{title}</span>{" "}
-            <span className={b("title-bottom")}>{filling}</span>
-          </h2>
-          <ul className={b("features-list")}>
-            <li className={b("features-item")}>
-              <b className={b("features-amount")}>{servingsCount}</b>
-              {` ${getServingsCountText(servingsCount)}`}
-            </li>
-            <li className={b("features-item")}>
-              <b className={b("features-amount")}>
-                {miceCount > 1 ? `${miceCount} ` : ``}
-              </b>
-              {`${getMiceCountText(miceCount)} в подарок`}
-            </li>
-            {additions.map(item => {
-              return <li>{item}</li>;
-            })}
-          </ul>
+class CardItem extends React.Component {
+  state = {
+    mouseEnter: false,
+    mouseLeaveAfterSelect: this.props.status === "selected"
+  };
+
+  handleMouseLeave = () => {
+    const { status } = this.props;
+    this.setState({
+      mouseEnter: false,
+      mouseLeaveAfterSelect: status === "selected"
+    });
+  };
+
+  handleMouseEnter = () => this.setState({ mouseEnter: true });
+
+  render() {
+    const {
+      type,
+      title,
+      filling,
+      servingsCount,
+      miceCount,
+      additions,
+      weight,
+      description,
+      status,
+      onSelect,
+      className
+    } = this.props;
+
+    const { mouseEnter, mouseLeaveAfterSelect } = this.state;
+
+    return (
+      <div className={`${b({ [status]: true })} ${className}`}>
+        <div
+          onClick={onSelect}
+          onMouseEnter={this.handleMouseEnter}
+          onMouseLeave={this.handleMouseLeave}
+          className={b("container")}
+        >
+          <div className={b("top-wrapper")}>
+            <p className={b("type")}>
+              {status === "selected" && mouseEnter && mouseLeaveAfterSelect
+                ? "Котэ не одобряет?"
+                : type}
+            </p>
+            <h2 className={b("title-wrapper")}>
+              <span className={b("title-top")}>{title}</span>{" "}
+              <span className={b("title-bottom")}>{filling}</span>
+            </h2>
+            <ul className={b("features-list")}>
+              <li className={b("features-item")}>
+                <b className={b("features-amount")}>{servingsCount}</b>
+                {` ${getServingsCountText(servingsCount)}`}
+              </li>
+              <li className={b("features-item")}>
+                <b className={b("features-amount")}>
+                  {miceCount > 1 ? `${miceCount} ` : ``}
+                </b>
+                {`${getMiceCountText(miceCount)} в подарок`}
+              </li>
+              {additions.map(item => {
+                return <li>{item}</li>;
+              })}
+            </ul>
+          </div>
+          <div className={b("image-wrapper")}>
+            <img
+              src="assets/images/picture@1x.png"
+              width="314"
+              height="272"
+              srcSet="assets/images/picture@2x.png 2x"
+              alt="Изображение продукта."
+            />
+          </div>
+          <p className={b("weight-wrapper")}>
+            <span className={b("weight-amount")}>
+              {weight.toLocaleString()}
+            </span>{" "}
+            кг
+          </p>
         </div>
-        <div className={b("image-wrapper")}>
-          <img
-            src="assets/images/picture@1x.png"
-            width="314"
-            height="272"
-            srcSet="assets/images/picture@2x.png 2x"
-            alt="Изображение продукта."
-          />
-        </div>
-        <p className={b("weight-wrapper")}>
-          <span className={b("weight-amount")}>{weight.toLocaleString()}</span>{" "}
-          кг
+        <p className={b("description")}>
+          {getDescription(status, filling, description)}
         </p>
       </div>
-      <p className={b("description")}>
-        {getDescription(status, filling, description)}
-      </p>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default CardItem;
